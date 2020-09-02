944daf49df5813d4a91589e04ee93e23d4e8cf57
Horacio Facundo Vega <horacio.facundo.vega@accenture.com>
2020-09-02 09:56:04 -0300

    Web API added

---
+++ webAPI/webAPI/models/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace webAPI.models
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options )
+            :base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
